67ceb6ab99355545f9e95f7baaf2c62db25713b5
shaj <kolobrod2002@yandex.ru>
2019-01-25 15:47:34 +0300

    Заготовка

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <iterator>
 #include <functional>
+#include <fstream>
 
 #include "oa_sort.h"
 
@@ -27,26 +28,62 @@ struct measure
 
 
 
-int main()
+void read_data(std::istream &is, std::vector<int> &v)
 {
+	std::string slen;
+	std::getline(is, slen);
+	v.clear();
+	v.reserve(std::stoi(slen));
+	for(std::string line; std::getline(is, line);)
 	{
-		std::vector<int> v {9, 3, 4, 6, 5, 7, 8, 2, 0, 1};
-
-		otusalg::insertion_sort(v);
-
-		std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
-		std::cout << std::endl;
+		v.emplace_back(std::stoi(line));
 	}
+}
 
+void test_vector(const std::vector<int> &v)
+{
+	int cnt = 0;
+	for(auto it = v.begin() + 1; it != v.end(); it++)
 	{
-		std::vector<int> v {9, 3, 4, 6, 5, 7, 8, 2, 0, 1};
-
-		otusalg::shell_sort(v.begin(), v.end(), std::less<int>());
-
-		std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
-		std::cout << std::endl;
+		if(*it < *(it-1)) cnt++;
 	}
+	if(cnt) std::cout << "array is NOT sorted\n";
+	else std::cout << "array is sorted" << std::endl;
+}
 
+int main(int argc, char const *argv[])
+{
+
+	if(argc > 1)
+	{
+		std::cout << "'" << argv[1] << "'\n";
+
+		std::vector<int> v;
+		std::ifstream fs;
+ 	    fs.open(argv[1], std::ios::in);
+ 
+	    if(fs.is_open())
+	    {
+	    	read_data(fs, v);
+	    	fs.close();
+
+	    	fs.open(argv[1]);
+	    	read_data(fs, v);
+	    	fs.close();
+	    	std::cout << "std::sort: " << measure<std::chrono::microseconds>::execution([&]()
+	    	{
+	    		std::sort(v.begin(), v.end(), std::less<int>());
+	    	}) << " us\n";
+	    	test_vector(v);
+
+
+	    }
+	    else
+	    {
+	    	std::cout << "File '" << argv[1] << "' does not exist" << std::endl;
+	    }
+	}
 
 	return 0;
 }
+
